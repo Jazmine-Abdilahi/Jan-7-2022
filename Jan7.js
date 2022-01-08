699ce67a06c7d8f804a7baa9f5f011a59de8320f
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-01-07 20:30:22 -0600

    Add files via upload

---
+++ Jan7.js
@@ -0,0 +1,41 @@
+// Jan 7 7:46 started ended
+// Video number 39 - 
+
+const friend1 = 'MrBallen'
+const friend2 = 'GamingWithKittens'
+const friend3 = 'MyNameIs Jabril'
+
+const friends = ['MrBallen', 'GamingWithKittens', 'MyNameIs Jabril']
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jaz';
+crossOriginIsolated.log(friends);
+
+
+const firstName = 'Jazmine';
+const Jazmine = ['firstName,' 'Jazmine', 2037 - 2010, 'student', friends];
+console.log(Jazmine);
+console.log(Jazmine.length);
+
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+const years (1990, 1963, 2002, 2010, 2018);
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[2]), calcAge(years[years.length - 1])];
+calcAge(years[years.length - 1])];
+console.log(ages);
+
